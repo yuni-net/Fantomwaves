bdb53ccde37d3621208a7196346e7d492ca05d64
yuni-net <yuni.net.liberty@gmail.com>
2015-09-15 22:35:28 +0900

    is_my_address was implemented

--- FantomWaves/fw_NetWork.h
+++ FantomWaves/fw_NetWork.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "fw_Array.h"
+
 namespace fw
 {
 
@@ -7,12 +9,20 @@ namespace fw
 	{
 	public:
 		static void init_ifneed();
+		static bool is_my_address(const unsigned long address);
 
 
 
 
 	private:
+		Array<unsigned long> myaddr_list;
+
+		static NetWork & get_instance();
 		NetWork();
+		void listup_alotof_myaddr();
 		~NetWork();
+
+
+
 	};
 }
