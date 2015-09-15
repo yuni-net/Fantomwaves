647f6923e34339882dea41e177d1d7eac74fb625
yuni-net <yuni.net.liberty@gmail.com>
2015-09-15 15:00:26 +0900

    UDP_cliant_interface

--- FantomWaves/fw_UDP_cliant.h
+++ FantomWaves/fw_UDP_cliant.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <fw_includes.h>
+#include <fw_thread.h>
+#include <fw_Array.h>
 
 namespace fw
 {
@@ -9,23 +11,73 @@ namespace fw
 	class UDP_cliant
 	{
 	public:
-		void connect_server(const unsigned short port, const Bindata & data, const int limit_time);
+
+		/**
+		 @brief サーバーへの接続を開始する。
+		 @param
+		      port: ポート番号を指定する。
+			  data: 自分が正規のクライアントであることを証明するデータを指定する。
+			  limit_time: サーバーからの応答を待つ制限時間を秒数で指定する。
+			  server_response: 送り返されてきたデータがこれと一致すれば、それをサーバーとみなす。
+		 */
+		void connect_server(
+			const unsigned short port,
+			const Bindata & data,
+			const int limit_time,
+			const Bindata & server_response);
+
+		/**
+		 @return
+		      true: 制限時間以内にサーバーが見つからなかった。
+			  false: まだ制限時間に達していないか、既にサーバーが見つかっている。
+		 */
 		bool did_timeout() const;
+
+		/**
+		 @return
+		      true: サーバーに接続した。
+			  false: サーバーに接続していない。
+		 */
+		bool did_connect_server() const;
+
+		/**
+		 @return
+		      true: 受信したデータがあり、まだ取り出されていない状態である。
+			  false: 受信したデータは無い。
+		 */
 		bool did_receive() const;
+
+		/**
+		 @return まだ取り出されていない受信データの数が返る。
+		 */
 		unsigned int num_received_datas() const;
+
+		/**
+		 @return 未取り出しの受信データから最も古いものを取り出して返す。
+		 */
 		Bindata pop_received_data();
-		void set_server_IP(const IP & server_IP);
+
+		/**
+		 @brief サーバーにデータを送信する。
+		 @param 送信したいデータを指定する。
+		 */
 		bool send(const Bindata & data) const;
 
+		/**
+		 @return サーバーのIPアドレスを返す。
+		 */
 		const IP & get_server_IP() const;
+
+		/**
+		 @return サーバーのポート番号を返す。
+		 */
 		unsigned short get_server_port() const;
 
 
 		// example
 #if 0
 		fw::UDP_cliant net;
-		net.connect_server(port, data, limit_time);
-		bool is_connecting_server = true;
+		net.connect_server(port, data, limit_time, server_response);
 
 		while (main_loop)
 		{
@@ -34,18 +86,9 @@ namespace fw
 				process_when_not_found_server();
 			}
 
-			if(is_connecting_server)
+			if(net.did_connect_server() == false)
 			{
 				show_connecting_animation();
-				if(net.did_receive())
-				{
-					fw::IP server_IP;
-					if(is_response_of_server(net.received_data(server_IP))
-					{
-						net.set_server_IP(server_IP);
-						is_connecting_server = true;
-					}
-				}
 			}
 			else
 			{
@@ -73,6 +116,11 @@ namespace fw
 		IP server_IP;
 		int limit_time;
 		sockaddr_in addr;
+		Bindata server_response;
+		fw::Thread thread_;
+		bool did_timeout_;
+		bool did_connect_server_;
+		RingQue<Bindata> received_datas;
 
 
 
@@ -84,5 +132,7 @@ namespace fw
 			 false: failed
 		 */
 		bool send_data(const Bindata & data);
+
+		static fw_thread_ find_server(void * param);
 	};
 }
--- FantomWaves_cpp/UDP_cliant.cpp
+++ FantomWaves_cpp/UDP_cliant.cpp
@@ -2,13 +2,20 @@
 #include <fw_UDP_cliant.h>
 #include <fw_NetWork.h>
 #include <fw_zeromemory.h>
+#include <fw_cast.h>
 
 namespace fw
 {
-	void UDP_cliant::connect_server(const unsigned short port, const Bindata & data, const int limit_time)
+	void UDP_cliant::connect_server(
+		const unsigned short port,
+		const Bindata & data,
+		const int limit_time,
+		const Bindata & server_response)
 	{
 		this->port = port;
 		this->limit_time = limit_time;
+		this->server_response = server_response;
+		received_datas.set_size(20);
 
 		NetWork::init_ifneed();
 		sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -18,33 +25,37 @@ namespace fw
 
 		set_addr_for_broadcast();
 		send_data(data);
+
+		did_timeout_ = false;
+		did_connect_server_ = false;
+		thread_.begin(find_server, this);
 	}
 
 	bool UDP_cliant::did_timeout() const
 	{
-		// todo
+		return did_timeout_;
+	}
+
+	bool UDP_cliant::did_connect_server() const
+	{
+		return did_connect_server_;
 	}
 
 	bool UDP_cliant::did_receive() const
 	{
-		// todo
+		return num_received_datas() > 0;
 	}
 
 	unsigned int UDP_cliant::num_received_datas() const
 	{
-		// todo
+		return received_datas.size();
 	}
 
 	Bindata UDP_cliant::pop_received_data()
 	{
-		// todo
+		return received_datas.pop();
 	}
 
-	void UDP_cliant::set_server_IP(const IP & server_IP)
-	{
-		this->server_IP = server_IP;
-		addr.sin_addr.s_addr = inet_addr(server_IP.to_ccharp());
-	}
 
 	bool UDP_cliant::send(const Bindata & data)
 	{
@@ -95,5 +106,70 @@ namespace fw
 		addr.sin_port = htons(port);
 	}
 
+
+	fw_thread_ UDP_cliant::find_server(void * param)
+	{
+		UDP_cliant & net = *pointer_cast<UDP_cliant *>(param);
+
+		sockaddr_in their_addr;
+		const int addr_len = sizeof(sockaddr_in);
+
+		fd_set fds, readfds;
+		FD_ZERO(&readfds);
+		FD_SET(net.sock, &readfds);
+
+		timeval timev;
+		timev.tv_sec = net.limit_time;
+		timev.tv_usec = 0;
+
+		while (true)
+		{
+			memcpy(&fds, &readfds, sizeof(fd_set));
+
+			const int result = select(0, &fds, NULL, NULL, &timev);
+			const int time_is_out = 0;
+			if (result == time_is_out)
+			{
+				break;
+			}
+
+			const int NOT_received = 0;
+			if (FD_ISSET(net.sock, &fds) == 0)
+			{
+				continue;
+			}
+
+			memset(message, 0, 1024);
+			recvfrom(
+				com_sock,
+				message,
+				1024,
+				0,
+				reinterpret_cast<sockaddr *>(&cliant_addr),
+				&addr_len);
+
+			std::cout << "You received a message:" << std::endl;
+			for (int i = 0; i < 48; ++i)
+			{
+				char code_str[256];
+				itoa(message[i], code_str, 10);
+				std::cout << message[i] << " (" << code_str << ")" << std::endl;
+			}
+
+			if (is_the_address_mine(cliant_addr.sin_addr.S_un.S_addr))
+			{
+				// 自分自身からのメッセージは無視する
+				std::cout << "It's from my self." << std::endl;
+				continue;
+			}
+
+			if (process_message(message, cliant_addr))
+			{
+				return 1;
+			}
+		}
+
+		return 0;
+	}
 
 }
