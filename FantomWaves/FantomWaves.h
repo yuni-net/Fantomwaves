685070638b4b93d09a6be4b9a94431449e515289
yuni-net <yuni.net.liberty@gmail.com>
2015-09-18 11:51:47 +0900

    added to FantomWaves.h

--- FantomWaves/FantomWaves.h
+++ FantomWaves/FantomWaves.h
@@ -121,6 +121,8 @@
 #include "fw_xrandom.h"
 #include "fw_xy2radian.h"
 #include "fw_NetWork.h"
+#include "fw_UDP_cliant.h"
+#include "fw_UDP_server.h"
 
 #ifndef FantomWaves_developing_
 	#ifdef _DEBUG
