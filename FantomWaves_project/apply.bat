1653290ff47c5c93571ab007a2b34a0b26f7f389
yuni-net <yuni.net.liberty@gmail.com>
2015-12-26 15:03:47 +0900

    remake project for vs2013

--- FantomWaves_project/apply.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\yuni\projects\cpp\Fantomwaves
-copy .\FantomWaves_project\Debug\FantomWaves.lib .\FantomWaves\FantomWavesd.lib /Y
-copy .\FantomWaves_project\Release\FantomWaves.lib .\FantomWaves\FantomWavesr.lib /Y
-pause
