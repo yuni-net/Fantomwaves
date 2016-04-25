f2e0fcb5c12cde87d45b90f38e13fb0c785b2192
yuni-net <yuni.net.liberty@gmail.com>
2016-04-25 11:32:23 +0900

    apply.bat was updated

--- apply.bat
+++ apply.bat
@@ -1,4 +1,5 @@
-cd C:\Users\yuni\projects\cpp\Fantomwaves
+echo applying...
 copy .\FantomWaves_project\Debug\FantomWaves.lib .\FantomWaves\FantomWavesd.lib /Y
 copy .\FantomWaves_project\Release\FantomWaves.lib .\FantomWaves\FantomWavesr.lib /Y
+echo finished!
 pause
